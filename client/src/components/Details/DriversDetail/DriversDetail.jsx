0f43761d8273453898b078a9888d86f464055075
Victor Sepulveda <victorseva123@gmail.com>
2024-01-01 14:16:19 -0500

    Actual 01/01/24

--- client/src/components/Details/DriversDetail/DriversDetail.jsx
+++ client/src/components/Details/DriversDetail/DriversDetail.jsx
@@ -1,26 +1,26 @@
 // Importar las imágenes y estilos necesarios
 import banner from '../../../images/banner.jpg';
-import './DogDetail.css';
+import './DriversDetail.css';
 
 // Componente funcional DogDetail que recibe un objeto 'dog' como prop
-function DogDetail({ dog }) {
+function DriverDetail({ driver }) {
 
   return (
-    <div className="dogDetail">
+    <div className="driverDetail">
       {/* Sección izquierda */}
       <div className="left">
         <div className="profile">
           {/* Imagen y nombre del perro */}
-          <div className="div_dog_profile">
-            <img className="image_dog_profile" src={dog?.image} alt="dog" />
+          <div className="div_driver_profile">
+            <img className="image_driver_profile" src={driver?.image} alt="driver" />
           </div>
-          <div className="div_dog_name">
-            <h2 className="name_dog">{dog?.name}</h2>
-            <span className="sobre_dog">SOBRE MI</span>
+          <div className="div_driver_name">
+            <h2 className="name_driver">{driver?.name}</h2>
+            <span className="sobre_driver">SOBRE MI</span>
           </div>
         </div>
 
-        {/* Tabla de características del perro */}
+        {/* Tabla de características del driver */}
         <div className="table_container">
           <table>
             <tbody>
@@ -31,8 +31,8 @@ function DogDetail({ dog }) {
               </tr>
               <tr>
                 <td className="table_caracteristicas">ALTURA</td>
-                <td className="table_datos">{dog?.height_min}</td>
-                <td className="table_datos">{dog?.height_max} Cm</td>
+                <td className="table_datos">{driver?.height_min}</td>
+                <td className="table_datos">{driver?.height_max} Cm</td>
               </tr>
               {/* Resto de características */}
               {/* ... */}
@@ -51,4 +51,4 @@ function DogDetail({ dog }) {
   )
 }
 
-export default DogDetail;
+export default DriverDetail;
