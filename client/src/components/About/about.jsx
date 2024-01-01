0eaef904a364e3b71d3fe720a0d5746611805a2f
Victor Sepulveda <victorseva123@gmail.com>
2024-01-01 13:53:00 -0500

    Actual 01/01/24

--- client/src/components/About/about.jsx
+++ client/src/components/About/about.jsx
@@ -8,13 +8,13 @@ const About = () => {
       <h1>BIENVENIDOS</h1>
       <p>
         Hola, mi nombre es Víctor Alfonso Sepúlveda y me emociona demasiado poder presentarles 
-        mi primer PI DOgs. Han sido días arduos de estudio y prueba y error; 
+        mi  PI Drivers. Han sido incontables días interminables y arduos de estudio, de prueba y error; 
         que hoy se ven reflejados en ella, es una muestra de mi visión y lo que se 
         puede ofrecer cuando están en ella. Mi fantástico gusto, mi pasión y amor por la tecnología
         me impulsaron a escoger esta porfesion. Creo que mi app puede ser usada 
         como herramienta para tod@s los que buscan mejorar el manejo del FRONT-END, BACK-END, DATABASE, etc. También estuve 
         muy al cuidado de crear una experiencia de usuario muy amena, intuitiva y limpia posible.
-        Tengo claro que esta app es solo el inicio y voy superándome a cada paso que doy, al mismo 
+        Tengo claro que es solo el inicio y voy superándome a cada paso que doy, al mismo 
         tiempo también superando las expectativas de los usuarios cuyos comentarios siempre serán 
         BIENVENIDOS y podrán agregar más valor a la app; para ello estoy dispuesto a recibir 
         sus comentarios o sugerencias, sin dudar enviarlas a través de victorseva123@gmail.com.
