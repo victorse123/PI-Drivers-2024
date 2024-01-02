ccd4e3cad1ec4c5b2cd6beb585d23d5de32645d4
Victor Sepulveda <victorseva123@gmail.com>
2024-01-02 18:58:43 -0500

    Actual 02/01/24

--- client/src/components/home/AllCards/Card/Card.jsx
+++ client/src/components/home/AllCards/Card/Card.jsx
@@ -1,31 +1,27 @@
+/* eslint-disable react/no-unknown-property */
 //import React from 'react';
-import imageDogDefault from '../../../../images/image-dog-default.jpg'
+import imageDriverDefault from '../../../../images/image-dog-default.jpg'
 import './Card.css';
 
-function Card({ image, name, temperament, weight_min, weight_max }) {
-  // Renderiza una tarjeta que muestra información sobre un perro
+function Card({ image, name, lastname  }) {
+  // Renderiza una tarjeta que muestra información sobre el driver
 
   return (
     <div className='card'>
       {/* Contenedor de la imagen */}
       <div className='div_image_card'>
-        {/* Imagen del perro, si no se proporciona, se muestra una imagen predeterminada */}
-        <img className='card_image_dog' src={image ? image : imageDogDefault} alt={name} />
+        {/* Imagen del driver , si no se proporciona, se muestra una imagen predeterminada */}
+        <img className='card_image_driver' src={image ? image : imageDriverDefault} alt={name} />
       </div>
       {/* Contenedor de la información */}
       <div className='div_info_card'>
-        {/* Nombre del perro */}
-        <span className='card_name_dog'>{name}</span>
+        {/* Nombre del driver */}
+        <span className='card_name_driver'>{name}</span>
         <div>
-          {/* Rango de peso del perro */}
-          <span className='card_weight_dog'>{weight_min} - {weight_max} KG</span>
+          {/* Apellido del driver */}
+          <span classlastName='card_lastname_driver'>{lastname}</span>
         </div>
-        <div>
-          {/* Temperamento del perro */}
-          <p className='card_temperament_dog'>
-            {temperament}
-          </p>
-        </div>
+        
       </div>
     </div>
   );
