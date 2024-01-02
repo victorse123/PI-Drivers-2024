0541f7c6d97b4c5a6b03e79a35b64ea3d3be9afa
Victor Sepulveda <victorseva123@gmail.com>
2024-01-02 18:43:01 -0500

    Actual 02/01/24

--- client/src/components/Header/SearchBar/SearchBar.jsx
+++ client/src/components/Header/SearchBar/SearchBar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { searchDogs } from "../../../Redux/actions/actions";
+import { searchDrivers } from "../../../Redux/actions/actions";
 import search_icon from "../../../images/Search.png";
 import './SearchBar.css';
 import { Link } from "react-router-dom";
@@ -8,22 +8,22 @@ import { Link } from "react-router-dom";
 
 function SearchBar() {
   const dispatch = useDispatch();
-  const [nameDog, setNameDog] = useState('');
-  const dogsHome = useSelector(state => state.dogsHome);
+  const [nameDriver, setNameDriver] = useState('');
+  const driversHome = useSelector(state => state.driversHome);
 
   // Función para manejar cambios en el input de búsqueda
   function handleChange(e) {
     const name = e.target.value;
-    setNameDog(name);
+    setNameDriver(name);
     // Realiza la búsqueda solo si hay un nombre escrito
     if (name) {
-      dispatch(searchDogs(name));
+      dispatch(searchDrivers(name));
     }
   }
 
   // Función para limpiar el campo de búsqueda
   function onSearch() {
-    setNameDog('');
+    setNameDriver('');
   }
 
   return (
@@ -38,11 +38,11 @@ function SearchBar() {
           type="text"
           placeholder="Buscar"
           onChange={handleChange}
-          value={nameDog}
+          value={nameDriver}
         />
         {/* Botón para limpiar el campo de búsqueda */}
         <button
-          className={nameDog.length > 0 ? "cleaner active" : "cleaner"}
+          className={nameDriver.length > 0 ? "cleaner active" : "cleaner"}
           onClick={() => onSearch()}
         >
           x
@@ -50,10 +50,10 @@ function SearchBar() {
       </div>
 
       {/* Mostrar resultados de la búsqueda */}
-      <div className={nameDog.length !== 0 ? "divSearchBar_Results active" : "divSearchBar_Results"}>
+      <div className={nameDriver.length !== 0 ? "divSearchBar_Results active" : "divSearchBar_Results"}>
         <div className="div_nameResult">
       
-          {nameDog && dogsHome.slice(0, 10).map((d) => (
+          {nameDriver && driversHome.slice(0, 10).map((d) => (
             <div key={d.id}>
               {/* Enlaces de los resultados de búsqueda */}
               <Link className="results" to={`/home/${d.id}`}>{d.name}</Link>
