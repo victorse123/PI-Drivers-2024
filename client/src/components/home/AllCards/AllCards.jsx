b109375c842b790eab4dbb8caa111260bef2ebee
Victor Sepulveda <victorseva123@gmail.com>
2024-01-02 19:08:49 -0500

    Actual 02/01/24

--- client/src/components/home/AllCards/AllCards.jsx
+++ client/src/components/home/AllCards/AllCards.jsx
@@ -1,19 +1,19 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getDogs } from '../../../Redux/actions/actions';
+import { getDrivers } from '../../../Redux/actions/actions';
 import Card from './Card/Card';
 import { Link } from 'react-router-dom';
 import Loader from '../Loader/Loader';
 import Pagination from '..//Pagination/Pagination';
 import './AllCards.css';
 
-function AllCards({ currentPage, setCurrentPage, dogsPerPage, indexOfFirstDog, indexOfLastDog }) {
+function AllCards({ currentPage, setCurrentPage, driversPerPage, indexOfFirstDriver, indexOfLastDriver }) {
   const dispatch = useDispatch();
-  const dogs = useSelector(state => state.dogs);
+  const drivers = useSelector(state => state.drivers);
   
-  // Obtiene los perros al cargar el componente
+  // Obtiene los drivers al cargar el componente
   useEffect(() => {
-    dispatch(getDogs());
+    dispatch(getDrivers());
   }, [dispatch]);
 
   // Función para manejar el cambio de página
@@ -21,18 +21,16 @@ function AllCards({ currentPage, setCurrentPage, dogsPerPage, indexOfFirstDog, i
     setCurrentPage(page);
   };
 
-  // Genera las tarjetas para mostrar los perros
+  // Genera las tarjetas para mostrar los drivers
   function generateCards() {
     return (
-      // Mapea los perros actuales y crea una tarjeta para cada uno
-      dogs.slice(indexOfFirstDog, indexOfLastDog).map((dog, i) => (
-        <Link to={`/home/${dog.id}`} key={i} className="link_all_cards">
+      // Mapea los drivers actuales y crea una tarjeta para cada uno
+      drivers.slice(indexOfFirstDriver, indexOfLastDriver).map((driver, i) => (
+        <Link to={`/home/${driver.id}`} key={i} className="link_all_cards">
           <Card 
-            image={dog.image} 
-            name={dog.name} 
-            weight_min={dog.weight_min}
-            weight_max={dog.weight_max}
-            temperament={dog.temperament} 
+            image={driver.image} 
+            name={driver.name} 
+            lastname={driver.lastname} 
           /> 
         </Link>
       ))
@@ -43,11 +41,11 @@ function AllCards({ currentPage, setCurrentPage, dogsPerPage, indexOfFirstDog, i
   return (
     <div className='AllCards_component'>
       <div className='AllCards'>
-        {dogs.length !== 0 ? generateCards() : <Loader />}
+        {drivers.length !== 0 ? generateCards() : <Loader />}
       </div>
       <Pagination 
-        dogsPerPage={dogsPerPage} 
-        totalPosts={dogs.length} 
+        driversPerPage={driversPerPage} 
+        totalPosts={drivers.length} 
         paginate={paginate} 
         currentPage={currentPage}
         setCurrentPage={setCurrentPage}
