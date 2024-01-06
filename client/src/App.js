b7a1331b396f4053c4af801c86d707ae0e7658a3
Victor Sepulveda <victorseva123@gmail.com>
2024-01-05 20:23:08 -0500

    Actual 05/01/24

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,24 @@ import { Route, Switch } from 'react-router-dom';
 import LandingPage from './/components/LandingPage/LandingPage';
 import Home from './/components/home/Home';
 import About from './/components/About/about';
-import NewDog from './/components/NewDog/NewDog';
+import NewDriver from './/components/NewDriver/NewDriver';
 import Filtros from './components/home/Filter/Filtros';
-import FiltroTemperamento from './components/home/Filter/FiltroTemperamento';
+import FiltroTeam from './components/home/Filter/FiltroTeam';
 import SearchBar from './components/Header/SearchBar/SearchBar';
 import Details from './/components/Details/Details';
 import './App.css';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import {  getDogs, getTemperament } from './Redux/actions/actions';
+import {  getDrivers, getTeam } from './Redux/actions/actions';
 
 function App() {
   const dispatch = useDispatch();
  
 
-  // Despues de que el componente esta cargado se realiza un dispath para  mostrar los primeros perros
+  // Despues de que el componente esta cargado se realiza un dispath para  mostrar los primeros drivers
   useEffect(() => {
-    dispatch( getDogs());
-    dispatch(getTemperament());
+    dispatch( getDrivers());
+    dispatch(getTeam());
   }, [dispatch]);
   
   return (
@@ -28,10 +28,10 @@ function App() {
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/home" component={Home} />
         <Route exact path="/about" component={About} />
-        <Route exact path="/new" component={NewDog} />
+        <Route exact path="/new" component={NewDriver} />
         <Route exact path="/searchBar" component={SearchBar} />
         <Route exact path="/filtro" component={Filtros} />
-        <Route exact path="/filtrotemperamento" component={FiltroTemperamento} />
+        <Route exact path="/filtroteam" component={FiltroTeam} />
         <Route exact path="/home/:id" component={Details} />
       </Switch>
     </div>
