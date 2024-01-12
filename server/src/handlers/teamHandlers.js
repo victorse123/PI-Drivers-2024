54d0471959133efc22c5cf0f4801ed810494d566
Victor Sepulveda <victorseva123@gmail.com>
2024-01-11 19:32:37 -0500

    Actual 11/01/24

--- server/src/handlers/teamHandlers.js
+++ server/src/handlers/teamHandlers.js
@@ -2,6 +2,7 @@ const { getAllTeams } = require("../controllers/teamController");
 // Controlador para obtener todos los equipos
 async function getAllTeamsHandler(req, res) {
   try {
+    // Obtener todos los equipos utilizando la función getAllTeams del controlador
     const allTeams = await getAllTeams();
     return res.status(200).json(allTeams);
   } catch (error) {
