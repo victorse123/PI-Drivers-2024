5688d17641e090190f756b52696e096a7ddd1603
Victor Sepulveda <victorseva123@gmail.com>
2024-01-11 20:03:17 -0500

    Actual 11/01/24

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -7,13 +7,31 @@ const {
 } = require("..//handlers/driversHandlers");
 
 const { getAllTeamsHandler } = require("../handlers/teamHandlers");
-
+// Ruta para obtener todos los conductores o conductores por nombre
 router.get("/drivers", getDriversHandler);
-
+// Ruta para obtener detalles de un conductor por ID
 router.get("/drivers/:id", getDetailDriverHandler);
-
+// Ruta para crear un nuevo conductor
 router.post("/drivers", postDriverHandler);
-
+// Ruta para obtener todos los equipos
 router.get("/teams", getAllTeamsHandler);
 
 module.exports = router;
+
+// const { Router } = require("express");: Importa el objeto Router de Express para definir rutas.
+
+// const router = Router();: Crea una instancia del enrutador de Express.
+
+// const { postDriverHandler, getDriversHandler, getDetailDriverHandler } = require("..//handlers/driversHandlers");: Importa los controladores necesarios para manejar las operaciones relacionadas con los conductores.
+
+// const { getAllTeamsHandler } = require("../handlers/teamHandlers");: Importa el controlador necesario para manejar la obtención de todos los equipos.
+
+// router.get("/drivers", getDriversHandler);: Ruta GET para obtener todos los conductores o conductores por nombre.
+
+// router.get("/drivers/:id", getDetailDriverHandler);: Ruta GET para obtener detalles de un conductor por ID.
+
+// router.post("/drivers", postDriverHandler);: Ruta POST para crear un nuevo conductor.
+
+// router.get("/teams", getAllTeamsHandler);: Ruta GET para obtener todos los equipos.
+
+// module.exports = router;: Exporta el enrutador para que pueda ser utilizado por la aplicación principal.
