2f21e07fba6b8b17513d06d340368006676fe3b7
Victor Sepulveda <victorseva123@gmail.com>
2024-01-11 19:57:20 -0500

    Actual 11/01/24

--- server/src/models/Team.js
+++ server/src/models/Team.js
@@ -1,20 +1,24 @@
 const { DataTypes } = require("sequelize");
-
+// Exportamos una función que define el modelo del equipo
 module.exports = (sequelize) => {
+  // Definimos el modelo "Team"
   sequelize.define(
     "Team",
     {
+      // ID del equipo, tipo entero, no nulo, clave primaria, autoincremental
       id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true,
       },
+      // Nombre del equipo, tipo cadena de texto, no nulo
       name: {
         type: DataTypes.STRING,
         allowNull: false,
       },
     },
-    { timestamps: false }
+    { timestamps: false } // Desactiva la creación automática de campos createdAt y updatedAt
+    
   );
 };
