a5b00f1a164ff4ca2ef477e5df933c721ab83f0a
Victor Sepulveda <victorseva123@gmail.com>
2024-01-14 12:15:27 -0500

    Actual 14/01/24

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -1,46 +1,48 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import Header from '../Header/Header';
-import DriverDetail from './DriversDetail/DriversDetail';
-import { GET_DETAILS, getDetail } from '../../Redux/actions/actions';
-import Footer from '../Footer/Footer';
-import './Details.css';
+// import { useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import Header from '../Header/Header';
+// import DriverDetail from './DriversDetail/DriversDetail';
+// import { GET_DETAILS, getDetail } from '../../Redux/actions/actions';
+// import Footer from '../Footer/Footer';
+// import './Details.css';
 
-function Details(props) {
-  // Obtiene el dispatch de Redux
-  const dispatch = useDispatch();
+// function Details(props) {
+//   // Obtiene el dispatch de Redux
+//   const dispatch = useDispatch();
 
-  // Obtiene la información del estado utilizando useSelector
-  const driver = useSelector(state => state.details);
+//   // Obtiene la información del estado utilizando useSelector
+//   const driver = useSelector(state => state.details);
 
-  // Obtiene el ID de los parámetros de la URL
-  const id = props.match.params.id;
+//   // Obtiene el ID de los parámetros de la URL
+//   const id = props.match.params.id;
 
-  // Dispara la acción para obtener detalles del perro al montar o actualizar el componente
-  useEffect(() => {
+//   // Dispara la acción para obtener detalles del perro al montar o actualizar el componente
+//   useEffect(() => {
 
-    dispatch(getDetail(id))
-    return () => {
-      dispatch({
-        type: GET_DETAILS,
-        payload: []
-      })
-    }
-  }, [dispatch, id])
+//     dispatch(getDetail(id))
+//     return () => {
+//       dispatch({
+//         type: GET_DETAILS,
+//         payload: []
+//       })
+//     }
+//   }, [dispatch, id])
 
-  return (
-    <div className='details_component'>
-      {/* Componente del encabezado */}
-      <Header />
+//   return (
+//     <div className='details_component'>
+//       {/* Componente del encabezado */}
+//       <Header />
 
-      {/* Componente de detalles del driver */}
-      {/* Pasa el driver específico del estado al componente DriverDetail */}
-      <DriverDetail driver={driver[0]} />
+//       {/* Componente de detalles del driver */}
+//       {/* Pasa el driver específico del estado al componente DriverDetail */}
+//       <DriverDetail driver={driver[0]} />
 
-      {/* Componente del pie de página */}
-      <Footer />
-    </div>
-  )
-}
+//       {/* Componente del pie de página */}
+//       <Footer />
+//     </div>
+//   )
+// }
 
-export default Details;
+// export default Details;
+
+
