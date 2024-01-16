c77054f3f482d3d4dbfc6e365528bb7434f38db0
Victor Sepulveda <victorseva123@gmail.com>
2024-01-16 07:01:35 -0500

    Actual 16/01/24

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -57,64 +57,64 @@
 // export default AllCards;
 
 
-import Card from "..//Card/Card";
-import defaultImage from '..//..//images/F1-3.jpg';
-import styles from "./Cards.css"
-import { useSelector } from "react-redux";
+// import Card from "..//Card/Card";
+// import defaultImage from '..//..//images/F1-3.jpg';
+// import styles from "./Cards.css"
+// import { useSelector } from "react-redux";
 
 
-function Cards({ onClose, currentPage, itemsPerPage, handlePageChange, setCurrentPage }) {
-  const indexOfLastDriver = currentPage * itemsPerPage;
-  const indexOfFirstDriver = indexOfLastDriver - itemsPerPage;
-  const allDriversCopy = useSelector((state) => state.allDriversCopy)
-  const currentDrivers = allDriversCopy;
-  const pageDrivers = currentDrivers.slice(indexOfFirstDriver, indexOfLastDriver);
+// function Cards({ onClose, currentPage, itemsPerPage, handlePageChange, setCurrentPage }) {
+//   const indexOfLastDriver = currentPage * itemsPerPage;
+//   const indexOfFirstDriver = indexOfLastDriver - itemsPerPage;
+//   const allDriversCopy = useSelector((state) => state.allDriversCopy)
+//   const currentDrivers = allDriversCopy;
+//   const pageDrivers = currentDrivers.slice(indexOfFirstDriver, indexOfLastDriver);
 
-  const handlePageInput = (e) => {
-    const newPage = parseInt(e.target.value, 10);
-    if (!isNaN(newPage)) {
-      // Si el valor es un número válido, actualiza el estado
-      setCurrentPage(newPage);
-    }
-  };
-  return (
-    <div > 
-<div className={styles.pagination}>
-        <button className={styles.button} onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
-          Previous
-        </button>
-        <div className={styles.inputWrapper}>
-      <input
-        type="number"
-        className={styles.inputBox}
-      value={currentPage}
-        onChange={handlePageInput}
-      /> 
-      <span className={styles.underline}></span>
-      </div>
-             <button className={styles.button} 
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={indexOfLastDriver >= currentDrivers.length}
-        >
-          Next
-        </button>
-      </div>
-      <div className={styles.cardContainer}> 
-       {pageDrivers.map((driver) => (
-         <Card
-           key={driver.id}
-           id={driver.id}
-           name={driver.name}
-           lastname={driver.surname || driver.lastname}
-           image={driver.image === '' ? defaultImage : driver.image}
-           teams={driver.teams}
-           onClose={onClose}
-         />
-       ))}
-     </div>
-    </div>
+//   const handlePageInput = (e) => {
+//     const newPage = parseInt(e.target.value, 10);
+//     if (!isNaN(newPage)) {
+//       // Si el valor es un número válido, actualiza el estado
+//       setCurrentPage(newPage);
+//     }
+//   };
+//   return (
+//     <div > 
+// <div className={styles.pagination}>
+//         <button className={styles.button} onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
+//           Previous
+//         </button>
+//         <div className={styles.inputWrapper}>
+//       <input
+//         type="number"
+//         className={styles.inputBox}
+//       value={currentPage}
+//         onChange={handlePageInput}
+//       /> 
+//       <span className={styles.underline}></span>
+//       </div>
+//              <button className={styles.button} 
+//           onClick={() => handlePageChange(currentPage + 1)}
+//           disabled={indexOfLastDriver >= currentDrivers.length}
+//         >
+//           Next
+//         </button>
+//       </div>
+//       <div className={styles.cardContainer}> 
+//        {pageDrivers.map((driver) => (
+//          <Card
+//            key={driver.id}
+//            id={driver.id}
+//            name={driver.name}
+//            lastname={driver.surname || driver.lastname}
+//            image={driver.image === '' ? defaultImage : driver.image}
+//            teams={driver.teams}
+//            onClose={onClose}
+//          />
+//        ))}
+//      </div>
+//     </div>
    
-  );
-}
+//   );
+// }
 
-export default Cards;
+// export default Cards;
