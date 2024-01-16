85a6f11744f74f5d7c0ec6d031ad4f99ab2f708a
Victor Sepulveda <victorseva123@gmail.com>
2024-01-16 07:29:44 -0500

    Actual 16/01/24

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -69,45 +69,45 @@
 // export default SearchBar;
 
 
-import React from "react";
-import { useState } from "react";
-import styles from "..//SearchBar/SearchBar.css"
+// import React from "react";
+// import { useState } from "react";
+// import styles from "..//SearchBar/SearchBar.css"
 
 
 
-export default function SearchBar(props){
-   const { handlePageChange, onSearch} = props; 
-const [name, setName] = useState("");
+// export default function SearchBar(props){
+//    const { handlePageChange, onSearch} = props; 
+// const [name, setName] = useState("");
 
 
-const handleChange = (event) => {
-setName(event.target.value)
-handlePageChange(1)
-}
+// const handleChange = (event) => {
+// setName(event.target.value)
+// handlePageChange(1)
+// }
 
-  const clearInput = () => {
-    setName('');
-  };
+//   const clearInput = () => {
+//     setName('');
+//   };
 
-return (
-    <div className={`${styles.inputWrapper} ${styles.inputWrapper}`}>
-    <div  className={`${styles.inputContainer} ${styles.inputContainer}`}>
-      <input
-       className={`${styles.inputBox} ${styles.inputBox}`} 
-        onChange={handleChange}
-        type="text"
-        placeholder="Driver's name..."
-        value={name}
-      />
-      {name && (
-        <div className={styles.clearButton} onClick={clearInput}>
-          ✕
-        </div>
-      )}
-    </div>
-    <button className={styles.button} onClick={() => onSearch(name)}>
-      Search
-    </button>
-  </div>
-)
-}
+// return (
+//     <div className={`${styles.inputWrapper} ${styles.inputWrapper}`}>
+//     <div  className={`${styles.inputContainer} ${styles.inputContainer}`}>
+//       <input
+//        className={`${styles.inputBox} ${styles.inputBox}`} 
+//         onChange={handleChange}
+//         type="text"
+//         placeholder="Driver's name..."
+//         value={name}
+//       />
+//       {name && (
+//         <div className={styles.clearButton} onClick={clearInput}>
+//           ✕
+//         </div>
+//       )}
+//     </div>
+//     <button className={styles.button} onClick={() => onSearch(name)}>
+//       Search
+//     </button>
+//   </div>
+// )
+// }
