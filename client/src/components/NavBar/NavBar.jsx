478d932f6575df1bbb4c2a41f084bfe4bf0a584e
Victor Sepulveda <victorseva123@gmail.com>
2024-01-16 07:28:31 -0500

    Actual 16/01/24

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -33,19 +33,19 @@
 
 
 
-import React from "react";
-import SearchBar from "../SearchBar/SearchBar";
-import { NavLink} from "react-router-dom";
-import styles from "./NavBar.module.css"
+// import React from "react";
+// import SearchBar from "../SearchBar/SearchBar";
+// import { NavLink} from "react-router-dom";
+// import styles from "./NavBar.module.css"
 
-export default function NavBar(props){
-const {onSearch, handlePageChange} = props;
+// export default function NavBar(props){
+// const {onSearch, handlePageChange} = props;
 
 
-    return (
-        <div className={styles.container}>
-<NavLink className={styles.navLink} to={"/create"} ><button className={`${styles.button}`}>Create a new driver</button></NavLink>
-<SearchBar className={styles.navLink} onSearch={onSearch} handlePageChange={handlePageChange}  />
-        </div>
-    )
-} 
+//     return (
+//         <div className={styles.container}>
+// <NavLink className={styles.navLink} to={"/create"} ><button className={`${styles.button}`}>Create a new driver</button></NavLink>
+// <SearchBar className={styles.navLink} onSearch={onSearch} handlePageChange={handlePageChange}  />
+//         </div>
+//     )
+// } 
